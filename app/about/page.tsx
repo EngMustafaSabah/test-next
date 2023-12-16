c8823cdaea7e0db80e7ed06b3a03c1b76231ac89
AhmedElshamy2 <ahmed.n.alshamy@gmail.com>
2023-12-17 01:03:46 +0200

    upload

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+
+const About = () => {
+  return (
+    <>
+        <Navbar/>
+    
+    </>
+  )
+}
+
+export default About
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbarr from './Navnar/Navbar'
+
+
+const page = () => {
+  
+  return (
+
+    <>
+      <Navbarr/>
+      <h1>hii</h1>
+    
+    </>
+  )
+}
+
+export default page
